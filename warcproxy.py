c6ac2a256689b5eed74c4f011b8aa678d50a9584
Alard <alard@example.com>
2012-08-26 14:50:24 +0200

    Work-in-progress with Tornado.

--- warcproxy.py
+++ warcproxy.py
@@ -1,125 +1,180 @@
-# http://stackoverflow.com/questions/3402694/twisted-web-proxy
-from twisted.internet import reactor
-from twisted.web import http
-from twisted.web.proxy import Proxy, ProxyRequest, ProxyClientFactory, ProxyClient
-from ImageFile import Parser
-from StringIO import StringIO
+import os
+import os.path
+import cPickle
+import re
+from collections import OrderedDict
+from contextlib import contextmanager
 
-# class InterceptingProxyClient(ProxyClient):
-#   def __init__(self, *args, **kwargs):
-#     ProxyClient.__init__(self, *args, **kwargs)
-
-#   def handleHeader(self, key, value):
-#     ProxyClient.handleHeader(self, key, value)
-
-#   def handleEndHeaders(self):
-#     ProxyClient.handleEndHeaders(self)
-
-#   def handleResponsePart(self, buffer):
-#     print buffer
-#     ProxyClient.handleResponsePart(self, buffer)
-
-#   def handleResponseEnd(self):
-#     ProxyClient.handleResponseEnd(self)
-
-# class InterceptingProxyClientFactory(ProxyClientFactory):
-#   protocol = InterceptingProxyClient
-
-# class InterceptingProxyRequest(ProxyRequest):
-#   protocols = {'http': InterceptingProxyClientFactory}
-#   ports = {"http" : 80}
-
-#   def requestReceived(self, command, path, version):
-#     # before
-#     print(command, path, version)
-#     ProxyRequest.requestReceived(self, command, path, version)
-
-#   def process(self):
-#     print("x")
-#     print(self.getRequestHostname())
-#     ProxyRequest.process(self)
-
-import sys
+import tornado.httpserver
+import tornado.ioloop
+import tornado.template
+import tornado.web
 
 from hanzo.warctools import WarcRecord
 from hanzo.httptools import RequestMessage, ResponseMessage
 
-records = {}
 
-filename = sys.argv[1]
+class WarcProxy(object):
+  def __init__(self):
+    self.warc_files = set()
+    self.indices = {}
 
-print "Indexing "+filename
-warc = WarcRecord.open_archive(sys.argv[1], gzip="auto")
-for (offset, record, errors) in warc.read_records(limit=None):
-  if record and record.type == WarcRecord.RESPONSE and record.content[0] == ResponseMessage.CONTENT_TYPE:
-    records[record.url] = offset
-warc.close()
-print "Indexed. Found "+str(len(records))+" URLs"
+  def load_warc_file(self, path):
+    if not path in self.warc_files:
+      self.warc_files.add(path)
+      idx_file = "%s.idx" % path
 
-class WarcProxyRequest(http.Request):
-  def process(self):
-    if self.method == "GET":
-      if self.uri == "http://warc/":
-        self.setResponseCode(200)
-        self.setHeader("Content-Type", "text/html")
-        self.write("<html><header><title>WARC index</title></header><body><h1>"+filename+"</h1><p>URLs saved:</p><ul>")
-        urls = records.keys()
-        urls.sort()
-        for url in urls:
-          self.write("<li><a href=\""+url+"\">"+url+"</a></li>")
-        self.write("</ul></body></html>")
+      if os.path.exists(idx_file) and os.path.getmtime(idx_file) >= os.path.getmtime(path):
+        with open(idx_file, "rb") as f:
+          records = cPickle.load(f)
+      
+      else:
+        records = OrderedDict()
+        warc = WarcRecord.open_archive(path, gzip="auto")
+        for (offset, record, errors) in warc.read_records(limit=None):
+          if record and record.type == WarcRecord.RESPONSE and record.content[0] == ResponseMessage.CONTENT_TYPE:
+            records[record.uri] = offset
+        warc.close()
 
-      elif self.uri in records:
-        warc = WarcRecord.open_archive(sys.argv[1], gzip="auto")
-        offset = records[self.uri]
-        warc.seek(offset)
+        with open(idx_file, "wb") as f:
+          cPickle.dump(records, f)
 
-        record = warc.__iter__().next()
+      print "Indexed. Found "+str(len(records))+" URLs"
 
-        if record:
-          message = ResponseMessage(RequestMessage())
-          leftover = message.feed(record.content[1])
-          message.close()
+      self.indices[path] = records
 
-          print("Serving "+self.uri)
-          self.setResponseCode(message.code)
-          for (name, value) in message.header.headers:
-            self.setHeader(name, value)
-          for (name, value) in record.headers:
-            self.setHeader(name, value)
-          self.write(message.get_body())
-        else:
-          self.setResponseCode(500)
-          self.write("Error retrieving record from WARC.\n")
+    return len(self.indices[path])
+
+  def unload_warc_file(self, path):
+    self.warc_files.discard(path)
+    del self.indices[path]
+
+  def iteruris(self):
+    for uris in self.indices.itervalues():
+      for uri in uris.iterkeys():
+        yield uri
+
+  @contextmanager
+  def warc_record_for_uri(self, uri):
+    found = False
+    for (path, offsets) in self.indices.iteritems():
+      if uri in offsets:
+        warc = WarcRecord.open_archive(path, gzip="auto")
+        warc.seek(offsets[uri])
+
+        for record in warc.read_records(limit=1, offsets=offsets[uri]):
+          found = True
+          yield record
 
         warc.close()
 
-      else:
-        self.setResponseCode(404)
-        self.setHeader("Content-Type", "text/html")
-        self.write("<html><header><title>URL not found in WARC</title></header><body><h1>URL not found in "+filename+"</h1><p><a href=\"http://warc/\">Visit http://warc/ for a list of URLs in this file</a></p></body></html>")
+    if not found:
+      yield None
+
+
+class WarcProxyWithWeb(object):
+  WEB_RE = re.compile(r"^http://(?P<host>warc)(?P<uri>/.*)$")
+
+  def __init__(self, proxy_handler, web_handler):
+    self.proxy_handler = proxy_handler
+    self.web_handler = web_handler
+
+  def __call__(self, request):
+    """Called by HTTPServer to execute the request."""
+    web_match = re.match(self.WEB_RE, request.uri)
+    if web_match:
+      request.host = web_match.group("host")
+      request.uri = web_match.group("uri")
+      request.path, sep, query = request.uri.partition("?")
+      self.web_handler.__call__(request)
 
     else:
-      self.setResponseCode(403)
-      self.write("Only GET requests are allowed.\n")
-
-    self.finish()
-
-class WarcProxy(Proxy):
-  requestFactory = WarcProxyRequest
-
-factory = http.HTTPFactory()
-factory.protocol = WarcProxy
-
-print
-print "Change the HTTP proxy of your browser to localhost:8000 and"
-print "then visit http://warc/"
-print
-
-reactor.listenTCP(8000, factory)
-reactor.run()
-
-warc.close()
+      with self.proxy_handler.warc_record_for_uri(request.uri) as record:
+        if record:
+          print "Serving %s from WARC" % request.uri
+          request.write(record[1].content[1])
+        else:
+          print "Could not find %s in WARC" % request.uri
+          request.write("HTTP/1.0 404 Not Found\r\nContent-Type: text/plain\r\nContent-Length: 12\r\n\r\nNot Found.\r\n")
+      request.finish()
 
 
+class FileBrowserHandler(tornado.web.RequestHandler):
+  def get(self):
+    cur_dir = self.get_argument("path", None)
+
+    if not cur_dir or not os.path.isdir(cur_dir):
+      self.redirect("?path=%s" % os.getcwd())
+      return
+
+    try:
+      files = os.listdir(cur_dir)
+    except OSError:
+      files = []
+
+    files.sort()
+    files = [{
+      "name":   f,
+      "path":   os.path.join(cur_dir, f),
+      "url":    ("?path=%s" % tornado.escape.url_escape(os.path.join(cur_dir, f))),
+      "isdir":  os.path.isdir(os.path.join(cur_dir, f)),
+      "iswarc": re.match(r".+\.warc(\.gz)?$", f)
+    } for f in files ]
+
+    cur_dir_parts = []
+    part = "X"
+    while part:
+      parent_dir, part = os.path.split(cur_dir)
+      cur_dir_parts.append({ "name":part, "path":cur_dir, "url":("?path=%s" % tornado.escape.url_escape(cur_dir)) })
+      cur_dir = parent_dir
+    cur_dir_parts.reverse()
+
+    self.render("browse.html", cur_dir_parts=cur_dir_parts, files=files)
+
+
+class MainHandler(tornado.web.RequestHandler):
+  def get(self):
+    self.write("Hello, world.")
+
+
+class WarcIndexHandler(tornado.web.RequestHandler):
+  def initialize(self, warc_proxy):
+    self.warc_proxy = warc_proxy
+
+  def get(self):
+    self.write("<ul>")
+    for uri in self.warc_proxy.iteruris():
+      self.write('<li><a href="%s">%s</a></li>' % (uri, uri))
+    self.write("</ul>")
+
+
+class WarcHandler(tornado.web.RequestHandler):
+  def initialize(self, warc_proxy):
+    self.warc_proxy = warc_proxy
+
+  def post(self, action):
+    if action == "load":
+      path = self.get_argument("path")
+      print "Loading " + path
+      num_records = self.warc_proxy.load_warc_file(path)
+      self.write("Indexed %s. Found %d URLs." % (path, num_records))
+    elif action == "unload":
+      print "Unloading " + self.get_argument("path")
+      self.warc_proxy.unload_warc_file(self.get_argument("path"))
+
+
+warc_proxy = WarcProxy()
+web_application = tornado.web.Application([
+  (r"/", MainHandler),
+  (r"/browse", FileBrowserHandler),
+  (r"/(load|unload)-warc", WarcHandler, { "warc_proxy":warc_proxy }),
+  (r"/index", WarcIndexHandler, { "warc_proxy":warc_proxy })
+], debug=True)
+
+my_application = WarcProxyWithWeb(warc_proxy, web_application)
+
+if __name__ == "__main__":
+  http_server = tornado.httpserver.HTTPServer(my_application)
+  http_server.listen(8000)
+  tornado.ioloop.IOLoop.instance().start()
 
